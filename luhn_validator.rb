621ce52a8cc51b50a546c76947859309c0e21dda
GSLiTW <coslove8866@gmail.com>
2022-03-11 01:06:05 +0800

    fix: part A rubocop offenses

--- luhn_validator.rb
+++ luhn_validator.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# Validates credit card number using Luhn Algorithm
 module LuhnValidator
   # Validates credit card number using Luhn Algorithm
   # arguments: none
@@ -5,18 +8,16 @@ module LuhnValidator
   # returns: true/false whether last digit is correct
   def validate_checksum
     nums_a = number.to_s.chars.map(&:to_i)
-    check_digit = nums_a.pop 
+    check_digit = nums_a.pop
 
     # TODO: use the integers in nums_a to validate its last check digit
-    sum_digits = nums_a.reverse.each_with_index.map do |element, index|
-      if index.odd?
-        element
-      else
-        (element*2 == 18) ? 9 : ((element*2) % 9)
-      end
+    sum_a = nums_a.reverse.each_with_index.map.reduce(0) do |sum, (element, index)|
+      sum + if index.odd?
+              element
+            else
+              (element * 2 == 18 ? 9 : ((element * 2) % 9))
+            end
     end
-
-    sum_a = sum_digits.reduce(:+)
 
     ((10 - (sum_a % 10)) % 10) == check_digit
   end
